502c978da7e6a1d321adbd35641c7c084a7621f5
Austin Flores <ctrl4ltdeleteme@gmail.com>
2019-01-25 08:50:52 +0100

    Set up base server and tests

--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
 require('dotenv').config()
+import logger from './src/config/logger'
+import { configure } from './src/config/webserver'
 const Koa = require('koa')
-const Router = require('koa-router')
-const send = require('koa-send')
-const serve = require('koa-static')
-const mount = require('koa-mount')
-const path = require('path')
 
-const app = new Koa()
-const router = new Router()
-app.use(mount('/assets', serve(path.resolve('front/assets'))))
-router.get('/', async (ctx) => await send(ctx, 'front/assets/index.html'))
+const app = configure(new Koa())
 
-app.use(router.routes())
-const port = process.env.PORT || '9090'
-app.listen(port)
+if (!module.parent) {
+  app.listen(app.context.port)
+
+  logger.info(
+    {
+      port: app.context.port,
+      environment: process.env.NODE_ENV,
+    },
+    ' (╯°□°）╯︵ ┻━┻',
+  )
+}
+
+export { app }
